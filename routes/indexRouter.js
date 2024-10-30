76fdaf1b2af814b5e54bc87b504372b695d4cb96
mayorr-star <immaboat11@gmail.com>
2024-10-30 12:08:18 +0000

    Add routes to get user dashboard and sign user out

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -6,5 +6,7 @@ const router = Router();
 router.get('/', indexController.getHomePage);
 router.get('/sign_up', indexController.getSignUpPage);
 router.get('/sign_in', indexController.getSignInPage);
+router.get('/dashboard', indexController.getDashboard);
+router.get('/sign_out', indexController.signOut);
 
 module.exports = router;
