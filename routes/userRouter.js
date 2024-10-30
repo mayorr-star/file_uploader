4ec4d90adbb819cc21492c7dc1cd5a207cd45164
mayorr-star <immaboat11@gmail.com>
2024-10-30 12:09:39 +0000

    Add a post route to handle user sign in requests

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,17 @@
-const { Router } = require('express');
-const userController = require('../controllers/userController.js');
+const { Router } = require("express");
+const userController = require("../controllers/userController.js");
+const passport = require("passport");
 
 const router = Router();
 
-router.post('/sign_up', userController.postUser);
+router.post("/sign_up", userController.postUser);
+router.post(
+  "/sign_in",
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/sign_in",
+    failureMessage: true
+  })
+);
 
 module.exports = router;
