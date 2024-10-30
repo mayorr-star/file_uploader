596f1023f75391e2e9d6d346fb9de7dda38d1664
mayorr-star <immaboat11@gmail.com>
2024-10-30 08:13:07 +0000

    Add new route to handle get requests to sign_in path

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,5 +5,6 @@ const router = Router();
 
 router.get('/', indexController.getHomePage);
 router.get('/sign_up', indexController.getSignUpPage);
+router.get('/sign_in', indexController.getSignInPage);
 
 module.exports = router;
