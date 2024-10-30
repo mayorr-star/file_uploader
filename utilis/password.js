dcdba9061a157a7dae80914c00bddb17912f2eb0
mayorr-star <immaboat11@gmail.com>
2024-10-30 12:11:31 +0000

    Make both function async.
    To await bycrypt.hash and compare functions result

--- utilis/password.js
+++ utilis/password.js
@@ -1,12 +1,13 @@
 const bycrypt = require("bcryptjs");
 
-const generatePassword = (password) => {
-  const saltHash = bycrypt.hash(password, 10);
+const generatePassword = async (password) => {
+  const saltHash = await bycrypt.hash(password, 10);
   return saltHash;
 };
 
-const verifyPassword = (password, userSalt) => {
-  return bycrypt.compare(password, userSalt);
+const verifyPassword = async (plainPassword, hashedPassword) => {
+  const result = await bycrypt.compare(plainPassword, hashedPassword);
+  return result;
 };
 
 module.exports = { generatePassword, verifyPassword };
