e892aa677bee08cde4f2605661244caefe5dbee5
mayorr-star <immaboat11@gmail.com>
2024-10-30 12:07:10 +0000

    Add getDashboard controller and signOut controller
    getDashboard displays dashboard after user successfully logs in.  signOut will log user out

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -9,10 +9,18 @@ const getSignUpPage = asyncHandler(async (req, res) => {
   res.render("signup");
 });
 
-const getSignInPage= asyncHandler(async(req, res) => {
-  res.render('signin');
-})
+const getSignInPage = asyncHandler(async (req, res) => {
+  const err_msg = req.session.messages || [];
+  req.session.messages = [];
+  res.render("signin", {err_msg: err_msg});
+});
 
+const getDashboard = asyncHandler(async (req, res) => {
+  res.render("dashboard");
+});
 
+const signOut = asyncHandler(async (req, res) => {
+  req.logOut(() => res.redirect("/sign_in"));
+});
 
-module.exports = { getHomePage, getSignUpPage, getSignInPage };
+module.exports = { getHomePage, getSignUpPage, getSignInPage, getDashboard, signOut };
