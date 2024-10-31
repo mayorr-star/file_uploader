f875df70606494d244c2f627e73970573d5318ae
mayorr-star <immaboat11@gmail.com>
2024-10-31 13:25:15 +0000

    Create getFile controller
     To get a specific file

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -9,7 +9,13 @@ const postFile = asyncHandler(async (req, res) => {
   const { originalname, size } = req.file;
   const folderId = req.params.folderId;
   await db.createFile(originalname, size, req.user.id, folderId);
-  res.redirect('/dashboard');
+  res.redirect("/dashboard");
 });
 
-module.exports = { renderUploadPage, postFile };
+const getFile = asyncHandler(async (req, res) => {
+  const fileId = req.params.fileId;
+  const file = await db.getUniqueFile(fileId, req.user.id);
+  res.render("file", {user: Boolean(req.user), file: file });
+});
+
+module.exports = { renderUploadPage, postFile, getFile };
