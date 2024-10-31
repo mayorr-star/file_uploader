fa47318207fdf24d88e5d784e87f02e8a2261170
mayorr-star <immaboat11@gmail.com>
2024-10-31 12:31:05 +0000

    Import file and use fileRouter

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const passport = require('./config/passport');
 const session = require('./config/session');
 const indexRouter = require('./routes/indexRouter');
 const userRouter = require('./routes/userRouter');
+const fileRouter = require('./routes/fileRouter');
 
 require('dotenv').config();
 
@@ -20,6 +21,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/user', userRouter);
+app.use('/file', fileRouter);
 app.use('/', indexRouter);
 
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`))
