3027142c067c6a6f28f1115c5ce8f3ae17ccd78d
mayorr-star <immaboat11@gmail.com>
2024-11-02 13:33:51 +0000

    Modify routes to include optional parentId
    Some folders will be sub folders and therefore have a parent

--- routes/folderRouter.js
+++ routes/folderRouter.js
@@ -4,7 +4,8 @@ const folderController = require("../controllers/folderController");
 const router = Router();
 
 router.get('/create', folderController.renderFolderForm);
-router.post('/create/new', folderController.postFolder);
+router.get('/create/:parentId?', folderController.renderFolderForm);
+router.post('/create/:parentId?/new', folderController.postFolder);
 router.get('/:folderId/open', folderController.getFolderContent)
 router.get('/:folderId/update', folderController.renderUpdateForm)
 router.post('/:folderId/update', folderController.updateFolder)
