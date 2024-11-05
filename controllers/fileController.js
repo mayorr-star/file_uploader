7f7173616760365b1f7b6311e2a638f8593a4333
mayorr-star <immaboat11@gmail.com>
2024-11-05 09:45:33 +0000

    Upload files to cloudinary storage

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,6 +1,9 @@
 const asyncHandler = require("express-async-handler");
 const db = require("../db/queries");
 const { NotFoundError } = require("../error handling/errors/errors");
+const upload = require("../config/multer");
+const cloudinary = require("../config/cloudinary");
+const generateUniquePublicId = require("../utilis/generateUniquePublicId");
 
 const renderUploadPage = asyncHandler(async (req, res) => {
   const folderId = req.params.folderId || null;
@@ -8,11 +11,27 @@ const renderUploadPage = asyncHandler(async (req, res) => {
   res.render("uploadfile", { user: Boolean(req.user), url: url });
 });
 
+const postFileToMulter = asyncHandler(async (req, res, next) => {
+  upload.single("file")(req, res, next);
+});
+
+const postFileToCloudinary = asyncHandler(async (req, res, next) => {
+  if (!req.file) return res.status(400);
+  const { originalname, mimetype } = req.file;
+  await cloudinary.uploader.upload(req.file.path, {
+    public_id: generateUniquePublicId(originalname),
+    resource_type: 'auto',
+    overwrite: true,
+  });
+  next();
+});
+
 const postFile = asyncHandler(async (req, res) => {
   const { originalname, size } = req.file;
   const folderId = req.params.folderId;
   await db.createFile(originalname, size, req.user.id, folderId);
-  res.redirect("/dashboard");
+  const route = folderId ? '/dashboard' : `/folder${folderId}/open`
+  res.redirect(route);;
 });
 
 const getFile = asyncHandler(async (req, res) => {
@@ -22,4 +41,16 @@ const getFile = asyncHandler(async (req, res) => {
   res.render("file", { user: Boolean(req.user), file: file });
 });
 
-module.exports = { renderUploadPage, postFile, getFile };
+const downloadFile = asyncHandler(async (req, res) => {
+  const { fileId } = req.params;
+  const file = await db.getUniqueFile(fileId);
+});
+
+module.exports = {
+  renderUploadPage,
+  postFile,
+  postFileToMulter,
+  postFileToCloudinary,
+  getFile,
+  downloadFile,
+};
