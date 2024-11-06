e3e4d3b6c5c04cf4bf342023a0cb3acf7e00bd50
mayorr-star <immaboat11@gmail.com>
2024-11-06 19:21:20 +0000

    Download file

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -4,6 +4,7 @@ const { NotFoundError } = require("../error handling/errors/errors");
 const upload = require("../config/multer");
 const cloudinary = require("../config/cloudinary");
 const generateUniquePublicId = require("../utilis/generateUniquePublicId");
+const axios = require("axios");
 
 const renderUploadPage = asyncHandler(async (req, res) => {
   const folderId = req.params.folderId || null;
@@ -20,7 +21,7 @@ const postFileToCloudinary = asyncHandler(async (req, res, next) => {
   const { originalname, mimetype } = req.file;
   const url = await cloudinary.uploader.upload(req.file.path, {
     public_id: generateUniquePublicId(originalname),
-    resource_type: 'auto',
+    resource_type: "auto",
     overwrite: true,
   });
   req.cloudinaryUrl = url;
@@ -31,8 +32,15 @@ const postFile = asyncHandler(async (req, res) => {
   const { originalname, size } = req.file;
   const folderId = req.params.folderId;
   const publicId = req.cloudinaryUrl.public_id;
-  await db.createFile(originalname, size, req.user.id, req.cloudinaryUrl.secure_url, publicId, folderId);
-  const route = folderId ? `/folder/${folderId}/open` : '/dashboard';
+  await db.createFile(
+    originalname,
+    size,
+    req.user.id,
+    req.cloudinaryUrl.secure_url,
+    publicId,
+    folderId
+  );
+  const route = folderId ? `/folder/${folderId}/open` : "/dashboard";
   res.redirect(route);
 });
 
@@ -46,24 +54,30 @@ const getFile = asyncHandler(async (req, res) => {
 const downloadFile = asyncHandler(async (req, res) => {
   const { fileId } = req.params;
   const file = await db.getUniqueFile(fileId);
-});
-
-const deleteFile = asyncHandler(async(req, res) => {
-  const {fileId } = req.params;
-  const file = await db.getUniqueFile(fileId);
-  const folderId = file.folderId;
-  await db.deleteFileFromBD(fileId);
-  const route = folderId ? `/folder/${folderId}/open` : '/dashboard'
+  const response = await axios.get(file.url, { responseType: "stream" });
+  res.setHeader("Content-Disposition", `attachment; filename="${file.name}"`);
+  res.setHeader("Content-Type", response.headers["content-type"]);
+  response.data.pipe(res);
+  const route = folderId ? `/folder/${folderId}/open` : "/dashboard";
   res.redirect(route);
 });
 
-const deleteFileFromCloudinary = asyncHandler(async(req, res, next) => {
+const deleteFile = asyncHandler(async (req, res) => {
   const { fileId } = req.params;
-  const file = await db.getUniqueFile(fileId)
+  const file = await db.getUniqueFile(fileId);
+  const folderId = file.folderId;
+  await db.deleteFileFromBD(fileId);
+  const route = folderId ? `/folder/${folderId}/open` : "/dashboard";
+  res.redirect(route);
+});
+
+const deleteFileFromCloudinary = asyncHandler(async (req, res, next) => {
+  const { fileId } = req.params;
+  const file = await db.getUniqueFile(fileId);
   const publicId = file.publicId;
   await cloudinary.uploader.destroy(publicId);
-  next()
-})
+  next();
+});
 
 module.exports = {
   renderUploadPage,
@@ -73,5 +87,5 @@ module.exports = {
   getFile,
   downloadFile,
   deleteFile,
-  deleteFileFromCloudinary
+  deleteFileFromCloudinary,
 };
